f35e0cf589f97cd2b816e9cac7396412bef4da81
Vadim Savostin <vsavdev@gmail.com>
2019-10-23 18:16:46 +0300

    answers controller specs

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
+
   let(:question) { create(:question) }
-  let(:answers) do
-    question
-    create_list(:answer, 5)
-  end
+  let(:answer) { create(:answer) }
 
   describe 'GET #index' do
+    let(:answers) { question create_list(:answer, 5) }
 
     before { get :index, params: { question_id: question } }
 
@@ -17,6 +16,110 @@ RSpec.describe AnswersController, type: :controller do
 
     it 'renders index view' do
       expect(response).to render_template :index
+    end
+  end
+
+  describe 'GET #show' do
+
+    before { get :show, params: { id: answer, question_id: question } }
+
+    it 'assigns the requested answer to @answer' do
+      expect(assigns(:answer)).to eq answer
+    end
+
+    it 'renders show view' do
+      expect(response).to render_template :show
+    end
+  end
+
+  describe 'GET #new' do
+
+    before { get :new, params: { question_id: question} }
+
+    it 'assigns a new Answer to @answer' do
+      expect(assigns(:answer)).to be_a_new(Answer)
+    end
+
+    it 'renders new view' do
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'GET #edit' do
+    before { get :edit, params: { id: answer, question_id: question } }
+    it 'assigns the requested answer to @answer' do
+      expect(assigns(:answer)).to eq answer
+    end
+
+    it 'renders edit view' do
+      expect(response).to render_template :edit
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves a new answer to database' do
+        expect { post :create, params: { question_id: question, answer: attributes_for(:answer) } }.to change(Answer, :count).by(1)
+      end
+      it 'redirects to show view' do
+        post :create, params: { question_id: question, answer: attributes_for(:answer) }
+        expect(response).to redirect_to question_answers_path
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save the answer to database' do
+        expect { post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid) } }.to_not change(Answer, :count)
+      end
+
+      it 're-renders new view' do
+        post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid) }
+        expect(response).to render_template :new
+      end
+    end
+  end
+
+  describe 'PATCH #update' do
+    context 'with valid attributes' do
+      it 'assigns the requested answer to @answer' do
+        patch :update, params: { question_id: question, id: answer, answer: attributes_for(:answer) }
+        expect(assigns(:answer)).to eq answer
+      end
+      it 'changes answer attributes' do
+        patch :update, params: { question_id: question, id: answer, answer: { body: 'new body' } }
+        answer.reload
+        expect(answer.body).to eq 'new body'
+      end
+
+      it 'redirects to updated question' do
+        patch :update, params: { question_id: question, id: answer, answer: attributes_for(:answer) }
+        expect(response).to redirect_to question_answer_path(answer)
+      end
+    end
+
+    context 'with invalid attributes' do
+      before { patch :update, params: { question_id: question, id: answer, answer: attributes_for(:answer, :invalid) } }
+      it 'does not change answer' do
+        answer.reload
+        expect(answer.body).to eq 'MyText'
+      end
+
+      it 're-renders edit view' do
+        expect(response).to render_template :edit
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    let!(:answer) { create(:answer) }
+
+    it 'deletes the answer' do
+      expect { delete :destroy, params: { question_id: question, id: answer } }.to change(Answer, :count).by(-1)
+    end
+
+    it 'redirects to index' do
+      delete :destroy, params: { question_id: question, id: answer }
+      expect(response).to redirect_to question_answers_path
     end
   end
 end
