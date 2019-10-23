48bd53bea28046eaee33be196d69e39aa6f1f71c
Vadim Savostin <vsavdev@gmail.com>
2019-10-23 18:16:13 +0300

    questions controller refactor

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,9 +6,7 @@ class QuestionsController < ApplicationController
     @questions = Question.all
   end
 
-  def show
-
-  end
+  def show; end
 
   def new
     @question = Question.new
@@ -23,9 +21,7 @@ class QuestionsController < ApplicationController
     end
   end
 
-  def edit
-
-  end
+  def edit; end
 
   def update
     if @question.update(question_params)
