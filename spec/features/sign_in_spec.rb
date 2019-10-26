68c8f96648145dd91791558985e280d9c22edee3
Vadim Savostin <vsavdev@gmail.com>
2019-10-26 16:06:40 +0300

    create sign_in feature spec

---
+++ spec/features/sign_in_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+feature 'User can sign in', %q{
+  In order to ask questions
+  As an authenticated user
+  I'd like to be able to sign in
+} do
+
+  given(:user) { create(:user) }
+
+  background { visit new_user_session_path }
+
+  scenario 'Registered user tries to sign in' do
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_on 'Log in'
+
+    #save_and_open_page
+    expect(page).to have_content 'Signed in successfully.'
+  end
+
+  scenario 'Unregistered user tries to sign in' do
+    fill_in 'Email', with: 'wrong_user@test.com'
+    fill_in 'Password', with: '123qwe'
+    click_on 'Log in'
+
+    expect(page).to have_content 'Invalid Email or password.'
+  end
+end
