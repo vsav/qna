516fb3c0a13baae868ee5a4ad199b5845fbc5b91
Vadim Savostin <vsavdev@gmail.com>
2019-10-28 21:38:45 +0300

    add answer model relations spec

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  it { should belong_to(:question).dependent(:destroy)}
+  it { should belong_to(:question) }
+  it { should belong_to(:author) }
   it { should validate_presence_of :body }
 end
