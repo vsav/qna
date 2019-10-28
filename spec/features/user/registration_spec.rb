2384d1030a07cc932170f2a91d7d45f148330ca5
Vadim Savostin <vsavdev@gmail.com>
2019-10-28 21:39:14 +0300

    user registration spec

---
+++ spec/features/user/registration_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+feature 'Guest can register', %q{
+  In order to sign in
+  As a guest
+  I'd like to be able to register
+} do
+
+  before { visit new_user_registration_path }
+
+  scenario 'Guest trying to register' do
+    fill_in 'Email', with: 'user@test.com'
+    fill_in 'Password', with: 'secret'
+    fill_in 'Password confirmation', with: 'secret'
+    click_on 'Sign up'
+
+    expect(page).to have_content 'Welcome! You have signed up successfully.'
+  end
+
+  scenario 'Guest trying to register with invalid data' do
+    fill_in 'Email', with: ''
+    click_on 'Sign up'
+
+    expect(page).to have_content "Email can't be blank"
+  end
+end
