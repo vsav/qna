9fe30bf1edf56cf05bc5c28f0ef0e83b5792dc1c
Vadim Savostin <vsavdev@gmail.com>
2019-10-29 22:56:45 +0300

    rename author to user

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
-  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  belongs_to :user
   validates :title, :body, presence: true
 end
