af2416113e68eb2b4efd31aa4b90144bde235652
Vadim Savostin <vsavdev@gmail.com>
2019-10-29 23:00:45 +0300

    remove alias

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   sequence :email do |n|
     "user#{n}@test.com"
   end
-  factory :user, aliases: [:author] do
+  factory :user do
     email
     password { '123qwe' }
     password_confirmation { '123qwe' }
