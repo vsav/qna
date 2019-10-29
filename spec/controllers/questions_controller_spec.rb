602fb79ec843fb33556af02a98a3b54ee6c92b18
Vadim Savostin <vsavdev@gmail.com>
2019-10-29 22:59:57 +0300

    questions_controller fix after review

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,9 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
-  let(:question) { create(:question, author: author) }
   let(:user) { create(:user) }
-  let(:author) { create(:user) }
+  let(:question) { create(:question, user: user) }
 
   describe 'GET #index' do
     let(:questions) { create_list(:question, 3) }
@@ -20,7 +19,9 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe 'GET #show' do
+
     before { get :show, params: { id: question } }
+
     it 'assigns the requested question to @question' do
       expect(assigns(:question)).to eq question
     end
@@ -31,8 +32,10 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe 'GET #new' do
+
     before { login(user) }
     before { get :new }
+
     it 'assigns a new Question to @question' do
       expect(assigns(:question)).to be_a_new(Question)
     end
@@ -43,8 +46,9 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe 'GET #edit' do
-    before { login(author) }
+    before { login(user) }
     before { get :edit, params: { id: question } }
+
     it 'assigns the requested question to @question' do
       expect(assigns(:question)).to eq question
     end
@@ -57,16 +61,25 @@ RSpec.describe QuestionsController, type: :controller do
   describe 'POST #create' do
     before { login(user) }
     context 'with valid attributes' do
+
       it 'saves a new question to database' do
         expect { post :create, params: { question: attributes_for(:question) } }.to change(Question, :count).by(1)
       end
+
       it 'redirects to show view' do
         post :create, params: { question: attributes_for(:question) }
         expect(response).to redirect_to assigns(:question)
       end
+
+      it 'question belongs to user' do
+        post :create, params: { question: attributes_for(:question) }
+        question = Question.all.order(created_at: :desc).first
+        expect(question.user).to eq user
+      end
     end
 
     context 'with invalid attributes' do
+
       it 'does not save the question to database' do
         expect { post :create, params: { question: attributes_for(:question, :invalid) } }.to_not change(Question, :count)
       end
@@ -78,13 +91,22 @@ RSpec.describe QuestionsController, type: :controller do
     end
   end
 
-  describe 'PATCH #update' do
-    before { login(author) }
+  describe 'POST #create as guest' do
+    it 'redirects to sign_in page' do
+      post :create, params: { question: attributes_for(:question) }
+      expect(response).to redirect_to new_user_session_path
+    end
+  end
+
+  describe 'PATCH #update as author' do
+    before { login(user) }
     context 'with valid attributes' do
+
       it 'assigns the requested question to @question' do
         patch :update, params: { id: question, question: attributes_for(:question) }
         expect(assigns(:question)).to eq question
       end
+
       it 'changes question attributes' do
         patch :update, params: { id: question, question: { title: 'new title', body: 'new body' } }
         question.reload
@@ -99,7 +121,9 @@ RSpec.describe QuestionsController, type: :controller do
     end
 
     context 'with invalid attributes' do
+
       before { patch :update, params: { id: question, question: attributes_for(:question, :invalid) } }
+
       it 'does not change question' do
         question.reload
         expect(question.title).to eq 'MyString'
@@ -112,9 +136,30 @@ RSpec.describe QuestionsController, type: :controller do
     end
   end
 
-  describe 'DELETE #destroy' do
-    before { login(author) }
-    let!(:question) { create(:question, author: author) }
+  describe 'PATCH #update as not author' do
+    context 'with valid attributes' do
+      it 'not changes question attributes' do
+        user2 = create(:user)
+        sign_in(user2)
+        patch :update, params: { id: question, question: { title: 'new title', body: 'new body' } }
+        question.reload
+        expect(question.title).to_not eq 'new title'
+        expect(question.body).to_not eq 'new body'
+        expect(flash[:alert]).to match('Question was not updated')
+      end
+    end
+  end
+
+  describe 'PATCH #update as guest' do
+    it 'redirects to sign_in page' do
+      post :create, params: { question: attributes_for(:question) }
+      expect(response).to redirect_to new_user_session_path
+    end
+  end
+
+  describe 'DELETE #destroy as author' do
+    before { login(user) }
+    let!(:question) { create(:question, user: user) }
 
     it 'deletes the question' do
       expect { delete :destroy, params: { id: question } }.to change(Question, :count).by(-1)
@@ -123,6 +168,17 @@ RSpec.describe QuestionsController, type: :controller do
     it 'redirects to index' do
       delete :destroy, params: { id: question }
       expect(response).to redirect_to questions_path
+    end
+  end
+
+  describe 'DELETE #destroy as not author' do
+    let(:user2) { create(:user) }
+    let!(:question) { create(:question, user: user2) }
+
+    it 'do not deletes the answer' do
+      sign_in(user)
+      expect { delete :destroy, params: { id: question } }.to change(Question, :count).by(0)
+      expect(flash[:alert]).to match('You do not have permission to do that')
     end
   end
 
