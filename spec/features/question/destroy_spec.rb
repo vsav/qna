816a29fef51bb94622369e0537a4ba5bfb7c0faf
Vadim Savostin <vsavdev@gmail.com>
2019-10-30 16:45:38 +0300

    fix after review, add edit with invalid params test

--- spec/features/question/destroy_spec.rb
+++ spec/features/question/destroy_spec.rb
@@ -7,6 +7,7 @@ feature 'User can delete own question', %q{
 } do
   describe 'Authenticated user' do
     given(:user) { create(:user) }
+    given(:user2) { create(:user) }
     given(:question) { create(:question, user: user) }
 
     scenario 'author trying to delete own question' do
@@ -19,7 +20,6 @@ feature 'User can delete own question', %q{
     end
 
     scenario 'user trying to delete other users question' do
-      user2 = create(:user)
       sign_in(user2)
       visit question_path(question)
       expect(page).to_not have_link 'Delete question'
