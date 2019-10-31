4aff04a935c578f3f0bc6f6d2f8f15ffe3ac4a0f
Vadim Savostin <vsavdev@gmail.com>
2019-10-31 16:45:16 +0300

    add updated content check

--- spec/features/question/edit_spec.rb
+++ spec/features/question/edit_spec.rb
@@ -5,10 +5,12 @@ feature 'User can edit own question', %q{
   As a question's author
   I'd like to be able to edit my question
 } do
+
+  given(:user) { create(:user) }
+  given(:user2) { create(:user) }
+  given(:question) { create(:question, user: user) }
+
   describe 'Authenticated user' do
-    given(:user) { create(:user) }
-    given(:user2) { create(:user) }
-    given(:question) { create(:question, user: user) }
 
     scenario 'author trying to edit own question' do
       sign_in(user)
@@ -17,8 +19,12 @@ feature 'User can edit own question', %q{
       expect(page).to have_field('Title', with: question.title)
       expect(page).to have_field('Body', with: question.body)
       fill_in 'Title', with: 'Another title'
+      fill_in 'Body', with: 'New body'
       click_on 'Ask question'
       expect(page).to have_content 'Question was successfully updated.'
+      expect(page).to have_content 'Another title'
+      expect(page).to have_content 'New body'
+
     end
 
     scenario 'author trying to edit own question with invalid params' do
@@ -38,7 +44,6 @@ feature 'User can edit own question', %q{
   end
 
   describe 'Unauthenticated user' do
-    given(:question) { create(:question) }
     scenario 'trying to edit question' do
       visit question_path(question)
       expect(page).to_not have_link 'Edit question'
