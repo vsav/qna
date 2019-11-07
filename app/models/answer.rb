d1cd18f33b0b9a940faf0d06b57aabb36a6ca04e
Vadim Savostin <vsavdev@gmail.com>
2019-11-07 20:18:57 +0300

    add transaction, bang method, change validation

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,21 +2,16 @@ class Answer < ApplicationRecord
   belongs_to :question
   belongs_to :user
   validates :body, presence: true
-  validate :best_uniqueness, on: :best
+  validates_uniqueness_of :best, if: :best , on: :create
 
   default_scope -> { order('best DESC, created_at') }
   scope :best, -> { where(best: true) }
 
-  def mark_best
-    previous_best = question.answers.find_by(best: true)
-    previous_best&.update!(best: false)
-    self.update!(best: true)
-  end
-
-  private
-
-  def best_uniqueness
-    errors.add(:answer, 'Only one answer can be marked as best') if question.answers.best.count > 1
+  def mark_best!
+    transaction do
+      question.answers.best.update_all(best: false)
+      update!(best: true)
+    end
   end
 
 end
