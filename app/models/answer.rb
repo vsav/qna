d1d3a7df489f227ce58f9a78d1c74ab6aea529e1
Vadim Savostin <vsavdev@gmail.com>
2019-11-07 05:09:19 +0300

    add mark_best, uniqueness validation

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,21 @@ class Answer < ApplicationRecord
   belongs_to :question
   belongs_to :user
   validates :body, presence: true
+  validate :best_uniqueness, on: :best
+
+  default_scope -> { order('best DESC, created_at') }
+  scope :best, -> { where(best: true) }
+
+  def mark_best
+    previous_best = question.answers.find_by(best: true)
+    previous_best&.update!(best: false)
+    self.update!(best: true)
+  end
+
+  private
+
+  def best_uniqueness
+    errors.add(:answer, 'Only one answer can be marked as best') if question.answers.best.count > 1
+  end
+
 end
