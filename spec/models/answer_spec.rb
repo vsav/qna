14c7a8bad37969f0703691bf0cab6e492183fc82
Vadim Savostin <vsavdev@gmail.com>
2019-11-07 21:14:49 +0300

    fix uniqueness validation

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -6,11 +6,15 @@ RSpec.describe Answer, type: :model do
   let!(:answer1) { create(:answer, question: question, user: user, best: true) }
   let!(:answer2) { create(:answer, question: question, user: user) }
   let!(:answer3) { create(:answer, question: question, user: user) }
+  let!(:question2) { create(:question, user: user) }
+  let!(:answer4) { create(:answer, question: question2, user: user, best: true) }
+  let!(:answer5) { create(:answer, question: question2, user: user) }
+  let!(:answer6) { create(:answer, question: question2, user: user) }
 
   it { should belong_to(:question) }
   it { should belong_to(:user) }
   it { should validate_presence_of :body }
-  it { should validate_uniqueness_of(:best).on(:create) }
+  it { should validate_uniqueness_of(:best).scoped_to(:question_id).on(:create) }
 
   describe 'answer mark_best' do
 
