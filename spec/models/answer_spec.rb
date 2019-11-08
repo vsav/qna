d563f55c23c9f9fd9d56b6d309fec3b6ef1a4db2
Vadim Savostin <vsavdev@gmail.com>
2019-11-08 10:15:22 +0300

    rename answer2 -> best_answer

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -4,12 +4,8 @@ RSpec.describe Answer, type: :model do
   let!(:user) { create(:user) }
   let!(:question) { create(:question, user: user) }
   let!(:answer1) { create(:answer, question: question, user: user) }
-  let!(:answer2) { create(:answer, question: question, user: user, best: true) }
+  let!(:best_answer) { create(:answer, question: question, user: user, best: true) }
   let!(:answer3) { create(:answer, question: question, user: user) }
-  let!(:question2) { create(:question, user: user) }
-  let!(:answer4) { create(:answer, question: question2, user: user, best: true) }
-  let!(:answer5) { create(:answer, question: question2, user: user) }
-  let!(:answer6) { create(:answer, question: question2, user: user) }
 
   it { should belong_to(:question) }
   it { should belong_to(:user) }
@@ -21,7 +17,7 @@ RSpec.describe Answer, type: :model do
     before do
       answer1.mark_best!
       answer1.reload
-      answer2.reload
+      best_answer.reload
     end
 
     it 'should mark answer2 as best' do
@@ -29,16 +25,14 @@ RSpec.describe Answer, type: :model do
     end
 
     it 'should change best answer' do
-      expect(answer2).to_not be_best
+      expect(best_answer).to_not be_best
       expect(answer1).to be_best
     end
   end
 
   describe 'best answer renders on top' do
     it 'best answer should be first in array' do
-      answers = question.answers
-      best_answer = answers.best.first
-      expect(question.answers).to eq([best_answer, answers[-2], answers[-1]])
+      expect(question.answers).to eq([best_answer, answer1, answer3])
     end
   end
 end
