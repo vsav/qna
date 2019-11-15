ca5c8bd67909e3b9788faf3cedeaa3049e4f8084
Vadim Savostin <vsavdev@gmail.com>
2019-11-15 07:43:44 +0300

    add attachments tests to create

--- spec/features/question/create_spec.rb
+++ spec/features/question/create_spec.rb
@@ -33,13 +33,15 @@ feature 'User can create question', %q{
       expect(page).to have_content "Title can't be blank"
     end
 
-    scenario 'asks a question with attached file' do
+    scenario 'asks a question with attached files' do
       fill_in 'Title', with: 'Test question'
       fill_in 'Body', with: 'text text text'
 
-      attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
+      attach_file 'Files', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
       click_on 'Ask question'
+
       expect(page).to have_link 'rails_helper.rb'
+      expect(page).to have_link 'spec_helper.rb'
     end
   end
 
