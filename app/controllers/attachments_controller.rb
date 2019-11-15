d243e6d15cf05ecc79d68e837f9cb6804a12ef04
Vadim Savostin <vsavdev@gmail.com>
2019-11-15 18:00:05 +0300

    move files render to partial, some refactoring

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,14 +1,8 @@
 class AttachmentsController < ApplicationController
   before_action :authenticate_user!
-  before_action :find_attachment, only: :destroy
 
   def destroy
-    @attachment.purge if current_user.is_author?(@attachment.record)
-  end
-
-  private
-
-  def find_attachment
     @attachment = ActiveStorage::Attachment.find(params[:id])
+    @attachment.purge if current_user.is_author?(@attachment.record)
   end
 end
