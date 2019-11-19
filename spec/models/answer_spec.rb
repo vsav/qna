112c22fbcb3cabea1eb2513bfe58244d3f890b3b
Vadim Savostin <vsavdev@gmail.com>
2019-11-20 02:20:23 +0300

    add relations to links

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -7,10 +7,13 @@ RSpec.describe Answer, type: :model do
   let!(:best_answer) { create(:answer, question: question, user: user, best: true) }
   let!(:answer3) { create(:answer, question: question, user: user) }
 
+  it { should have_many(:links).dependent(:destroy) }
   it { should belong_to(:question) }
   it { should belong_to(:user) }
   it { should validate_presence_of :body }
   it { should validate_uniqueness_of(:best).scoped_to(:question_id).on(:create) }
+
+  it { should accept_nested_attributes_for :links }
 
   it 'have many attached files' do
     expect(Answer.new.files).to be_an_instance_of(ActiveStorage::Attached::Many)
