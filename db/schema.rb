2dfd0596f56b2ef3079f29224ce43edaf6224b96
Vadim Savostin <vsavdev@gmail.com>
2019-11-21 21:15:16 +0300

    create reward model and specs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_15_152256) do
+ActiveRecord::Schema.define(version: 2019_11_21_030526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,16 @@ ActiveRecord::Schema.define(version: 2019_11_15_152256) do
     t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
+  create_table "rewards", force: :cascade do |t|
+    t.string "title"
+    t.bigint "question_id"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id"], name: "index_rewards_on_question_id"
+    t.index ["user_id"], name: "index_rewards_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -82,4 +92,6 @@ ActiveRecord::Schema.define(version: 2019_11_15_152256) do
   add_foreign_key "answers", "questions"
   add_foreign_key "answers", "users"
   add_foreign_key "questions", "users"
+  add_foreign_key "rewards", "questions"
+  add_foreign_key "rewards", "users"
 end
