d30c3771fd911588bdfbd2de8f984f873e153b63
Vadim Savostin <vsavdev@gmail.com>
2019-11-21 05:52:25 +0300

    add links destroy feature to models

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,7 +1,7 @@
 class Answer < ApplicationRecord
   has_many_attached :files
   has_many :links, dependent: :destroy, as: :linkable
-  accepts_nested_attributes_for :links, reject_if: :all_blank
+  accepts_nested_attributes_for :links, reject_if: :all_blank, allow_destroy: true
   belongs_to :question
   belongs_to :user
   validates :body, presence: true
