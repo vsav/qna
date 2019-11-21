8f02eba486b1b501912f7516264b2d4f22de9c35
Vadim Savostin <vsavdev@gmail.com>
2019-11-21 10:44:04 +0300

    update for rewards

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,8 +1,10 @@
 class Question < ApplicationRecord
+  has_one :reward, dependent: :destroy
   has_many :answers, dependent: :destroy
   has_many :links, dependent: :destroy, as: :linkable
   has_many_attached :files
   accepts_nested_attributes_for :links, reject_if: :all_blank, allow_destroy: true
+  accepts_nested_attributes_for :reward, reject_if: :all_blank, allow_destroy: true
   belongs_to :user
   validates :title, :body, presence: true
 end
