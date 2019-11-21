92256ad873ed45a1f2040479e89865ee77c62300
Vadim Savostin <vsavdev@gmail.com>
2019-11-21 21:21:19 +0300

    create rewards_controller tests

---
+++ spec/controllers/rewards_controller_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe RewardsController, type: :controller do
+  let(:user) { create(:user) }
+  let(:question) { create(:question, user: user) }
+  let!(:rewards) { create_list(:reward, 3, question: question, user: user) }
+
+  describe 'GET #index' do
+    context 'authenticated user' do
+      before do
+        login(user)
+        get :index, params: { user_id: user.id }
+      end
+
+
+      it 'populates an array with user rewards' do
+        expect(assigns(:rewards)).to match_array(rewards)
+      end
+
+      it 'renders index view' do
+        expect(response).to render_template :index
+      end
+    end
+
+    context 'unauthenticated user' do
+      before { get :index, params: { user_id: user.id } }
+
+      it 'do not populate an array with user rewards' do
+        expect(assigns(:badges)).to be_nil
+      end
+
+      it 'redirects to login page' do
+        expect(response).to redirect_to(new_user_session_path)
+      end
+
+    end
+  end
+end
