9ea8fd1ea5008dd6f0f984752006d9441fc69784
Vadim Savostin <vsavdev@gmail.com>
2019-11-26 12:42:10 +0300

    move url_validator to file and tests

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,20 +1,15 @@
 class Link < ApplicationRecord
-  URL_FORMAT = /(^$)|(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix
+
+  include ActiveModel::Validations
+
   GIST_URL = /https:\/\/gist.github.com\/\w+\/(\w+)/
 
   belongs_to :linkable, polymorphic: true
   validates :name, :url, presence: true
-  validate :validate_url_format, on: :create
+  validates :url, url: true
 
   def gist?
     url.match(GIST_URL)
   end
 
-  private
-
-  def validate_url_format
-    unless url =~ URL_FORMAT
-      errors.add(:url, 'must be a valid URL format')
-    end
-  end
 end
---
+++ app/validators/url_validator.rb
@@ -0,0 +1,15 @@
+class UrlValidator < ActiveModel::EachValidator
+
+  URL_FORMAT = /(^$)|(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix
+
+  def validate_each(record, attribute, value)
+    return true if value =~ URL_FORMAT
+    record.errors[attribute] << url_error_message
+  end
+
+  private
+
+  def url_error_message
+    options[:message] || 'must be a valid URL format'
+  end
+end
