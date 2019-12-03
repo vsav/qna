68aef5a801893d5ca495f7de2c07da6bb7c6185f
Vadim Savostin <vsavdev@gmail.com>
2019-12-03 13:34:47 +0300

    add routes for concerns

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :questions do
-    resources :answers, shallow: true, only: [:create, :update, :destroy] do
+
+  concern :votable do
+    member do
+      post :like, :dislike
+      delete :unvote
+    end
+  end
+
+  resources :questions, concerns: :votable do
+    resources :answers, concerns: :votable, shallow: true, only: [:create, :update, :destroy] do
       patch :set_best, on: :member
     end
   end
