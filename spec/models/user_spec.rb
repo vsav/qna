181e7da3eddc80e9f59a7a8900977f117daa65d7
Vadim Savostin <vsavdev@gmail.com>
2019-12-03 13:38:26 +0300

    voting methods tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,12 +7,16 @@ RSpec.describe User, type: :model do
   it { should have_many(:questions).dependent(:destroy) }
   it { should have_many(:answers).dependent(:destroy) }
   it { should have_many(:rewards).dependent(:destroy) }
+  it { should have_many(:votes).dependent(:destroy) }
 
   describe 'user is_author?' do
     let(:user) { create(:user) }
     let(:user2) { create(:user) }
+    let(:user3) { create(:user) }
     let(:question) { create(:question, user: user) }
     let(:answer) { create(:answer, question: question, user: user) }
+    let!(:vote1) { create(:vote, :like, user: user2, votable: question) }
+    let!(:vote2) { create(:vote, :dislike, user: user3, votable: question) }
 
     context 'user is question author' do
       it 'should return true' do
@@ -35,6 +39,28 @@ RSpec.describe User, type: :model do
     context 'user is not answer author' do
       it 'should return true' do
         expect(user2).to_not be_is_author(answer)
+      end
+    end
+
+    context 'user can rate votable' do
+      it 'should return true if user2 liked question' do
+        expect(user2).to be_liked(question)
+      end
+
+      it 'should return true if user3 disliked question' do
+        expect(user3).to be_disliked(question)
+      end
+    end
+
+    context 'user voted for votable' do
+      it 'should return true if user2 voted for question' do
+        expect(user2).to be_voted(question)
+      end
+      it 'should return true if user3 voted for question' do
+        expect(user3).to be_voted(question)
+      end
+      it 'should return true if user2 do not voted for answer' do
+        expect(user2).to_not be_voted(answer)
       end
     end
 
