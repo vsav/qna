fb75b33bc23919b8c6da5debbfb151a3f0ef39f8
Vadim Savostin <vsavdev@gmail.com>
2019-12-16 09:22:46 +0300

    add publish methods

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,6 +4,7 @@ class QuestionsController < ApplicationController
 
   before_action :authenticate_user!, except: [:index, :show]
   before_action :find_question, only: [:show, :edit, :update, :destroy]
+  after_action :publish_question, only: :create
 
   def index
     @questions = Question.all
@@ -12,6 +13,7 @@ class QuestionsController < ApplicationController
   def show
     @answer = Answer.new
     @answer.links.new
+    gon.question_id = @question.id
   end
 
   def new
@@ -24,6 +26,7 @@ class QuestionsController < ApplicationController
     @question = Question.new(question_params)
     @question.user = current_user
     if @question.save
+      gon.question_id = @question.id
       redirect_to @question, notice: 'Question was successfully created.'
     else
       render :new
@@ -50,6 +53,23 @@ class QuestionsController < ApplicationController
     end
   end
 
+  def publish_question
+    return if @question.errors.any?
+
+    ActionCable.server.broadcast(
+        'questions',
+        html: html(@question),
+        question: @question
+    )
+  end
+
+  def html(question)
+    ApplicationController.render(
+        partial: 'questions/question_header',
+        locals: { question: question }
+    )
+  end
+
   private
 
   def find_question
@@ -58,7 +78,6 @@ class QuestionsController < ApplicationController
 
   def question_params
     params.require(:question).permit(:title, :body, files: [], links_attributes: [:id, :name, :url, :_destroy],
-                                     reward_attributes: [:id, :title, :image, :_destroy])
+                                     reward_attributes: [:title, :image])
   end
-
 end
