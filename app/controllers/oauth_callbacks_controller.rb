822109172f2910ba63e8c52e55c5dd715caa1fc0
Vadim Savostin <vsavdev@gmail.com>
2019-12-19 07:59:35 +0300

    create oauth controllers

---
+++ app/controllers/oauth_callbacks_controller.rb
@@ -0,0 +1,25 @@
+class OauthCallbacksController < Devise::OmniauthCallbacksController
+
+  before_action :oauth
+
+  def github; end
+
+  def vkontakte; end
+
+  private
+
+  def oauth
+    auth = request.env['omniauth.auth']
+    @user = User.find_for_oauth(auth)
+    if @user&.confirmed?
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: auth.provider.capitalize) if is_navigational_format?
+    elsif @user
+      session[:provider] = auth.provider
+      session[:uid] = auth.uid
+      redirect_to new_user_confirmation_path, notice: 'Please provide your email'
+    else
+      redirect_to root_path, alert: 'Something went wrong'
+    end
+  end
+end
