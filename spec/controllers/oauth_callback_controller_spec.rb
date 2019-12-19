e354dfe8045a1f55c2ea6eaeda3c862b39a20c82
Vadim Savostin <vsavdev@gmail.com>
2019-12-19 08:05:18 +0300

    controllers tests

---
+++ spec/controllers/oauth_callback_controller_spec.rb
@@ -0,0 +1,98 @@
+require 'rails_helper'
+
+RSpec.describe OauthCallbacksController, type: :controller do
+
+  describe 'Github' do
+    before do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      @request.env['omniauth.auth'] = mock_auth_hash(:github, email: 'user@test.com')
+    end
+
+    let!(:oauth_data) { mock_auth_hash(:github, email: 'user@test.com') }
+
+    it 'finds user from oauth data' do
+      allow(request.env).to receive(:[]).and_call_original
+      allow(request.env).to receive(:[]).with('omniauth.auth').and_return(oauth_data)
+      expect(User).to receive(:find_for_oauth).with(oauth_data)
+      get :github
+    end
+
+    context 'user esxists' do
+      let!(:user) { create(:user) }
+
+      before do
+        allow(User).to receive(:find_for_oauth).and_return(user)
+        get :github
+      end
+
+      it 'logins user if it exists' do
+        expect(subject.current_user).to eq user
+      end
+      it 'redirects to root path' do
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context 'user does not exists' do
+      before do
+        allow(User).to receive(:find_for_oauth)
+        get :github
+      end
+
+      it 'redirects to root path if user does not exists' do
+        expect(response).to redirect_to root_path
+      end
+      it 'does not login user if it not exists' do
+        expect(subject.current_user).to_not be
+      end
+    end
+  end
+
+  describe 'Vkontakte' do
+    before do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      @request.env['omniauth.auth'] = mock_auth_hash(:vkontakte)
+    end
+
+    let!(:oauth_data) { mock_auth_hash(:vkontakte) }
+
+    it 'finds user from oauth data' do
+      allow(request.env).to receive(:[]).and_call_original
+      allow(request.env).to receive(:[]).with('omniauth.auth').and_return(oauth_data)
+      expect(User).to receive(:find_for_oauth).with(oauth_data)
+      get :vkontakte
+    end
+
+    context 'user esxists' do
+      let!(:user) { create(:user) }
+
+      before do
+        allow(User).to receive(:find_for_oauth).and_return(user)
+        get :vkontakte
+      end
+
+      it 'logins user if it exists' do
+        expect(subject.current_user).to eq user
+      end
+
+      it 'redirects to root path' do
+        expect(response).to redirect_to root_path
+      end
+
+    end
+
+    context 'user does not exists' do
+      before do
+        allow(User).to receive(:find_for_oauth)
+        get :vkontakte
+      end
+
+      it 'redirects to root path if user does not exists' do
+        expect(response).to redirect_to root_path
+      end
+      it 'does not login user if it not exists' do
+        expect(subject.current_user).to_not be
+      end
+    end
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,7 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'capybara/email/rspec'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -38,6 +39,7 @@ RSpec.configure do |config|
   config.include ControllerHelpers, type: :controller
   config.include FeatureHelpers, type: :feature
   config.include ActiveStorageHelpers
+  config.include OmniauthMacros
 
   Capybara.javascript_driver = :selenium_chrome_headless
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
@@ -47,7 +49,7 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
-
+  OmniAuth.config.test_mode = true
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
