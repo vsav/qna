c614d3294af2c14ec077a6f8c419481343e1fce5
Vadim Savostin <vsavdev@gmail.com>
2020-01-10 04:54:40 +0300

    create api/v1/base_controller

---
+++ app/controllers/api/v1/base_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::BaseController < ApplicationController
+
+  before_action :doorkeeper_authorize!
+  skip_before_action :set_gon_user
+
+  private
+
+  def current_resource_owner
+    @current_resource_owner ||= User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+  end
+
+  def current_ability
+    @current_ability ||= Ability.new(current_resource_owner)
+  end
+end
