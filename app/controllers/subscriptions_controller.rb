e4684b6a5256ceab5e4726a90f2f75088e9bd5af
Vadim Savostin <vsavdev@gmail.com>
2020-01-11 03:18:51 +0300

    create subscription model and controller

---
+++ app/controllers/subscriptions_controller.rb
@@ -0,0 +1,15 @@
+class SubscriptionsController < ApplicationController
+  before_action :authenticate_user!
+
+  authorize_resource
+
+  def create
+    @question = Question.find(params[:question_id])
+    @subscription = @question.subscriptions.create(user: current_user)
+  end
+
+  def destroy
+    @subscription = Subscription.find(params[:id])
+    @subscription.destroy
+  end
+end
---
+++ db/migrate/20200110181310_create_subscriptions.rb
@@ -0,0 +1,10 @@
+class CreateSubscriptions < ActiveRecord::Migration[6.0]
+  def change
+    create_table :subscriptions do |t|
+      t.references :user, foreign_key: true, null: false
+      t.references :question, foreign_key: true, null: false
+      t.timestamps
+    end
+    add_index(:subscriptions, %i[user_id question_id], unique: true)
+  end
+end
