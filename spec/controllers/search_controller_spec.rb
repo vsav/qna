de378830e9cddc311448a42daac626c2d0e5ca6f
Vadim Savostin <vsavdev@gmail.com>
2020-01-15 05:26:47 +0300

    fix after review

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -6,20 +6,25 @@ RSpec.describe SearchController, type: :controller do
     SearchService::RESOURCES.each do |resource|
       it "search in #{resource}" do
         expect(resource.constantize).to receive(:search).with('test')
-        get :results, params: { input: 'test', resource: resource }
+        get :results, params: { query: 'test', resource: resource }
       end
 
       it 'renders results view' do
         expect(resource.constantize).to receive(:search).with('test')
-        get :results, params: { input: 'test', resource: resource }
+        get :results, params: { query: 'test', resource: resource }
         expect(response).to render_template :results
       end
 
     end
 
-    it 'search entire site' do
+    it 'search entire site if resource nil' do
       expect(ThinkingSphinx).to receive(:search).with('test')
-      get :results, params: { input: 'test' }
+      get :results, params: { query: 'test', resource: nil }
+    end
+
+    it 'search entire site if resource not in list' do
+      expect(ThinkingSphinx).to receive(:search).with('test')
+      get :results, params: { query: 'test', resource: 'not_in_list' }
     end
   end
 end
