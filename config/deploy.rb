a78c83155f83e59f1a0b22d8b7dcc2079480c843
Vadim Savostin <vsavdev@gmail.com>
2020-01-16 19:54:18 +0300

    Deploy configuration

---
+++ config/deploy.rb
@@ -0,0 +1,16 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.11.2"
+
+set :application, "QnA"
+set :repo_url, "git@github.com:vsav/qna.git"
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, "/home/deploy/qna"
+set :deploy_user, "deploy"
+
+# Default value for :linked_files is []
+append :linked_files, "config/database.yml", "config/master.key"
+
+# Default value for linked_dirs is []
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "storage"
+
---
+++ config/deploy/production.rb
@@ -0,0 +1,37 @@
+# server-based syntax
+# ======================
+# Defines a single server with a list of roles and multiple properties.
+# You can define all roles on a single server, or split them:
+
+server "163.172.164.93", user: "deploy", roles: %w{app db web}, primaty: true
+set :rails_env, :production
+
+
+# Custom SSH Options
+# ==================
+# You may pass any option but keep in mind that net/ssh understands a
+# limited set of options, consult the Net::SSH documentation.
+# http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start
+#
+# Global options
+# --------------
+set :ssh_options, {
+  keys: %w(/Users/VS/.ssh/id_rsa),
+  forward_agent: true,
+  auth_methods: %w(publickey password),
+  port: 2222
+}
+#
+# The server-based syntax can be used to override options:
+# ------------------------------------
+# server "example.com",
+#   user: "user_name",
+#   roles: %w{web app},
+#   ssh_options: {
+#     user: "user_name", # overrides user setting above
+#     keys: %w(/home/user_name/.ssh/id_rsa),
+#     forward_agent: false,
+#     auth_methods: %w(publickey password)
+#     # password: "please use keys"
+#   }
+
