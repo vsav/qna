d450b25fb80f2be6c13fb7c6561f83edd4198231
Vadim Savostin <vsavdev@gmail.com>
2020-01-16 19:07:40 +0300

    action cable config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = ["http://163.172.164.93"]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -61,6 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "qna_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: '163.172.164.93', port: 2222 }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
